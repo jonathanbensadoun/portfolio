1645cd45752021da5bc5c42d367c47a1721becc2
jonathanBensadoun <jonathan.ben-sadoun@oclock.school>
2024-04-28 13:32:23 +0200

    modification des card

--- src/components/CardProject/CardProject.jsx
+++ src/components/CardProject/CardProject.jsx
@@ -1,20 +1,59 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 export default function CardProject({ title, description, image, link }) {
+  const [openCard, setOpenCard] = useState(false);
+
   return (
-    <div className="flex flex-col justify-center items-center">
-      <h1 className="text-1xl md:text-2xl">{title}</h1>
-      <h2 className="mx-4 mt-4 text-justify md:text-1xl">{description}</h2>
-      <div className="w-60 h-60 overflow-hidden ">
-        <img
-          src={image}
-          alt={title}
-          className=" top-0 left-0 w-full h-full object-cover"
-        />
+    <div
+      className={`hover:shadow-4xl m-4  `}
+      onClick={() => setOpenCard(!openCard)}
+    >
+      <div
+        className={` bg-purple-800 pt-2 px-2 ${
+          openCard ? 'rounded-t' : 'rounded'
+        }  transition-all ease-in-out duration-300  `}
+      >
+        <div className="w-60 h-60 ">
+          <img
+            src={image}
+            alt={title}
+            className={`rounded top-0 left-0 w-60 h-60 object-cover`}
+          />
+        </div>
+
+        <div
+          className={`flex justify-center pt-1 transition-all ease-in-out duration-300 ${
+            openCard ? 'opacity-0' : 'opacity-100'
+          }`}
+        >
+          <div
+            className={`rounded bottom-0  h-1 bg-white transition-all ease-in-out duration-300 ${
+              openCard ? 'w-40' : 'w-14'
+            } `}
+          ></div>
+        </div>
       </div>
-      <a href={link} target="_blank" rel="noreferrer">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Voir le projet
-        </button>
-      </a>
+      <div
+        className={`flex flex-col justify-center items-center  bottom-0 left-0 right-0 bg-purple-800  rounded-b transition-all duration-500 ease-in-out transform ${
+          openCard ? 'translate-y-0 opacity-100' : '-translate-y-full opacity-0'
+        }`}
+      >
+        <h1 className="text-1xl md:text-2xl">{title}</h1>
+        <h2 className="mx-4 mt-4 text-justify md:text-1xl">{description}</h2>
+        <Link to={`/${link}`}>
+          <button className="bg-purple-500 hover:bg-purple-700 text-white mt-5 font-bold py-2 px-4 rounded mb-2">
+            Voir le projet
+          </button>
+        </Link>
+        <div className="flex justify-center">
+          <div
+            className={` rounded bottom-0 left-0 w-40 h-1 bg-white  ${
+              openCard ? 'w-40' : 'w-14'
+            }`}
+          ></div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,33 +1,34 @@
+import { useState } from 'react';
 import CardProject from '../CardProject/CardProject';
 
 export default function Projects() {
   return (
     <div
       id="projects"
-      className="flex flex-col justify-center items-center border-t-2 border-solid border-white-500 flex flex-col justify-center "
+      className="flex flex-col justify-center items-center border-t-2 border-solid border-white-500 flex flex-col justify-center items-center "
     >
-      <h1 className="text-3xl md:text-6xl">Projects</h1>
+      <h1 className="text-3xl mt-4 md:text-6xl">Projects</h1>
       <h2 className="mx-4 mt-4 text-justify md:text-3xl">
         Vous trouverez ici quelques-uns des projets personnels
       </h2>
-      <div className="flex flex-wrap justify-center items-center space-x-4 space-y-4  ">
+      <div className="flex flex-wrap justify-center items-center  mt-4 ">
         <CardProject
           title="Osurvivors"
           description="Description du projet 1"
           image="./img/Osurvivors3.png"
-          link="https://www.google.com"
+          link=""
         />
         <CardProject
           title="Osurvivors"
           description="Description du projet 1"
-          image="./img/Osurvivors3.png"
-          link="https://www.google.com"
+          image="./img/Osurvivors2.png"
+          link=""
         />
         <CardProject
           title="Osurvivors"
           description="Description du projet 1"
-          image="./img/Osurvivors3.png"
-          link="https://www.google.com"
+          image="./img/Osurvivors1.png"
+          link=""
         />
       </div>
     </div>
