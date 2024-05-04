82cac0fae2e8408065ebf7947ac1a9b7c121129a
jonathanBensadoun <jonathan.ben-sadoun@oclock.school>
2024-05-04 16:18:49 +0200

    modification des couleur pour le footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,8 +6,8 @@ export default function Footer() {
     <div
       className={`flex justify-center text-xs pb-20 shadow-xl  p-6 md:pb-6 md:text-base  ${
         isLightMode
-          ? 'bg-secondary text-text'
-          : 'bg-secondaryDark  text-textDark'
+          ? 'bg-secondaryDark text-text'
+          : 'bg-secondary  text-textDark'
       }`}
     >
       <p>© Droits d'auteur 2024.</p>
